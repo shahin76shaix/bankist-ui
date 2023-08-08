33617341d8528e69caf330dc1f615df5de0fa1ab
shahin76shaix <shaix.official@gmail.com>
2023-08-08 13:37:15 +0330

    lecture 6

--- script.js
+++ script.js
@@ -384,3 +384,22 @@ labelBalance.addEventListener('click', function () {
   }
 });
 */
+/*
+//Numeric Seprator:
+
+//287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+//Wrong cases
+console.log(Number('230000'));
+console.log(Number.parseInt('230_000'));
+*/
