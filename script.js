de9bf42f07d93a278ee91663b5dea2c34c4b1b6c
shahin76shaix <shaix.official@gmail.com>
2023-08-10 14:33:04 +0330

    added more dates

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Shahin Seraj',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -514,7 +514,7 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 */
-
+/*
 //Operations with dates:
 const future = new Date(2037, 10, 19, 15, 23);
 console.log(future);
@@ -525,3 +525,4 @@ const calcDaysPassed = (date1, date2) =>
 
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
 console.log(days1);
+*/
